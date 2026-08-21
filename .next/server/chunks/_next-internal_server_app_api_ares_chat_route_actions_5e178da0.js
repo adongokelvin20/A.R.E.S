853378ccc59a41bf97deee806f4d52157508e594
Kelvin Ayinbisa <adongokelvin20@gmail.com>
2026-08-21 15:10:47 +0000

    Remove .next build folder again

--- .next/server/chunks/_next-internal_server_app_api_ares_chat_route_actions_5e178da0.js
+++
@@ -1,3 +0,0 @@
-module.exports=[8317,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_ares_chat_route_actions_5e178da0.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mVnbFqZQQqPjtUyago+kcvft1QDGZIXj6hOGqu0gPDo=\"\n}"
